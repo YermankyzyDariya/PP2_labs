3708911b6742f47b308656389b0eebea26934493
YermankyzyDariya <d_yermankyzy@kbtu.kz>
2025-03-03 12:13:32 +0500

    wow

--- defence/1.py
+++ defence/1.py
@@ -4,12 +4,5 @@ print(even_numbers)  # [2, 4, 6, 8, 10]
 
 
 def first_uppercase(text):
-    """Возвращает первую заглавную букву из строки"""
-    for char in text:
-        if char.isupper():
-            return char
-    return None  # Если заглавных букв нет
-
-print(first_uppercase("hello World"))  # ➝ 'W'
-print(first_uppercase("python"))       # ➝ None
+    return text[0].isupper() if text else False
 print(first_uppercase("I love You"))   # ➝ 'I'
